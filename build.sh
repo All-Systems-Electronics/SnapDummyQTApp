2228b645e8e86c0013f699d963ea52a7cb583da7
King-Swood <darren@all-systems.com.au>
2024-10-15 11:28:12 +1100

    *Modified build script so it now longer installs and runs

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Build dummy application
+rm -r ./build
+mkdir build
+cd build
+qmake ../DummyApp/DummyApp.pro
+make
+cd ../
+
+# Copy binary into release folder
+rm -r ./release
+mkdir -p ./release/bin
+cp ./build/DummyApp ./release/bin/
+
+# Clean the build so that we are starting afresh
+snapcraft clean
+
+# Build the snap
+snapcraft
+
+# If we got this far, we can install and run the app
+# sudo snap install --devmode ./dummyapp-snap_0.1_amd64.snap
+# dummyapp-snap.dummyapp
